bf7956d3d77c29d1dd0297c61210de3d148f8322
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-05-23 16:31:23 +0530

    backend project initialized

---
+++ README.md
@@ -0,0 +1,1 @@
+# noobstack-API
---
+++ src/test/java/com/example/noobstack/NoobstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noobstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoobstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
